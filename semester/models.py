64e95700dbfbbdaf9d2f5e0b1973139123acdeb7
郑力煽 <602868253@qq.com>
2019-12-28 16:33:26 +0800

    Feat: finish semester module
    Close #7

--- semester/models.py
+++ semester/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Semester(models.Model):
+    STATE = (
+        (0, '正在进行'),
+        (1, '已结束')
+    )
+    period_semester = models.IntegerField()
+    theme = models.TextField()
+    introduction = models.TextField()
+    state = models.IntegerField(choices=STATE, default=0)
