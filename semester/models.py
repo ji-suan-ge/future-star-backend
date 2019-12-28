277145b48ce4a62266c823c9e490ff0a6bbf45be
郑力煽 <602868253@qq.com>
2019-12-28 17:33:52 +0800

    Docs(semester): modify semester module
    Ref #7

--- semester/models.py
+++ semester/models.py
@@ -1,14 +1,24 @@
-from django.db import models
+"""
+models
 
-# Create your models here.
+:author: lishanZheng
+:date: 2019/12/28
+"""
+from django.db import models
 
 
 class Semester(models.Model):
+    """
+    semester
+
+    :author: lishanZheng
+    :date: 2019/12/28
+    """
     STATE = (
         (0, '正在进行'),
         (1, '已结束')
     )
-    period_semester = models.IntegerField()
-    theme = models.TextField()
-    introduction = models.TextField()
+    period_semester = models.IntegerField()  # 期数
+    subject = models.TextField()  # 主题
+    introduction = models.TextField()  # 介绍
     state = models.IntegerField(choices=STATE, default=0)
--- semester/tests.py
+++ semester/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+"""
+tests
 
-# Create your tests here.
+:author: lishanZheng
+:date: 2019/12/28
+"""
+# from django.test import TestCase
