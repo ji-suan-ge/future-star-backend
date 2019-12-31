304dde4192b7e1a081fba99109004844b91b6715
郑力煽 <602868253@qq.com>
2019-12-31 15:24:16 +0800

    Feat(administrator): finish admin login
    Ref #16

--- administrator/views.py
+++ administrator/views.py
@@ -4,4 +4,38 @@ views
 :author: gexuewen
 :date: 2019/12/28
 """
-# from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from administrator import code
+from util import result_uitl
+from .serializers import AdministratorSerializer
+from .models import Administrator
+
+
+@csrf_exempt
+def login(request):
+    """
+    login
+
+    :author: lishanZheng
+    :date: 2019/12/31
+    """
+    if request.method == 'POST' and request.POST:
+        account = request.POST.get('account')
+        password = request.POST.get('password')
+        correct_password = ''
+        admin = ''
+        try:
+            admin = Administrator.objects.get(account=account)
+            correct_password = admin.password
+        except Administrator.DoesNotExist:
+            return result_uitl.error(error_code=code.ADMIN_NOT_EXIST, message='管理员不存在')
+        if correct_password == password:
+            if request.session.get('is_login') is None:
+                admin.password = ''
+                request.session['is_login'] = True
+                admin = AdministratorSerializer(admin).data
+                request.session['admin'] = admin
+                return result_uitl.success(admin)
+            return result_uitl.error(error_code=code.IS_LOGIN, message='已登陆')
+        return result_uitl.error(error_code=code.INCORRECT_PASSWORD, message='密码错误')
+    return result_uitl.error(error_code=code.EMPTY_REQUEST, message='请求体空')
