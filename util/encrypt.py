87690e89f8bba11ad5885b20509f6168d72e7f51
郑力煽 <602868253@qq.com>
2019-12-31 22:12:07 +0800

    Fix(util): lint fix
    Ref #25

--- util/encrypt.py
+++ util/encrypt.py
@@ -1,7 +1,19 @@
+"""
+encrypt
+
+:author: lishanZheng
+:date: 2019/12/30
+"""
 import hashlib
 
 
 def encrypt(data):
+    """
+    加密
+
+    :author: lishanZheng
+    :date: 2019/12/30
+    """
     sha256 = hashlib.sha256()
     sha256.update(data.encode())
     res = sha256.hexdigest()
@@ -9,6 +21,12 @@ def encrypt(data):
 
 
 def compare(before, after):
+    """
+    比较
+
+    :author: lishanZheng
+    :date: 2019/12/30
+    """
     before = encrypt(before)
     if before == after:
         return True
