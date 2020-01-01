98b1d2ff1bc34ca607d459a4fa7bc49612d6ed20
xuewenG <gexuewen@outlook.com>
2020-01-01 17:35:37 +0800

    Feat(activity): add activity api
    Close #30

--- activity/urls.py
+++ activity/urls.py
@@ -8,8 +8,12 @@ from django.urls import path
 
 from activity import views
 
-LIST_ACTIVITY = 'list_activity'
+LIST_ACTIVITY = 'list'
+ADD_ACTIVITY = 'add'
+# DELETE_ACTIVITY = ''
 
 urlpatterns = [
     path(LIST_ACTIVITY, views.ActivityList.as_view()),
+    path(ADD_ACTIVITY, views.AddActivity.as_view()),
+    # path('delete/<int:id>', views.DeleteActivity.as_view())
 ]
