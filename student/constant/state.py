fc33e57832cca91db80bdac9cb7b17906a6e584b
郑力煽 <602868253@qq.com>
2020-01-03 15:46:59 +0800

    Refactor(student): refactor student test
    Ref #61

--- student/constant/state.py
+++ student/constant/state.py
@@ -2,9 +2,11 @@
 state
 
 :author: lishanZheng
-:date: 2020/01/01
+:date: 2020/01/03
 """
-# 0 -不再是校友 ｜ 1 -还未毕业的校友 ｜2 -校友
+# 校友
 VALID = 2
+# 还未毕业的校友
 NOT_GRADUATE = 1
+# 不再是校友
 INVALID = 0
--- student/urls.py
+++ student/urls.py
@@ -9,7 +9,6 @@ from django.urls import path
 from student import views
 
 urlpatterns = [
-    # path('login', views.login),
     path('student', views.StudentList.as_view()),
     path('student/<int:primary_key>', views.StudentDetailViewSet.as_view())
 ]
