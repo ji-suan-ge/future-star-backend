673acb995bcb24cef72d38490a6cea72218d3e65
xuewenG <gexuewen@outlook.com>
2020-01-03 14:48:52 +0800

    Test(activity): join activity test
    Close #55

--- activity/test/test_activity_student_list.py
+++ activity/test/test_activity_student_list.py
@@ -4,54 +4,27 @@ activity student list test
 :author: gexuewen
 :date: 2020/01/02
 """
-from django.test import TestCase
-
 from activity.constant.activity_student_state import WAIT_FOR_PAY
-from activity.models import Activity, ActivityStudent
-from activity.test.generate.activity import get_activity_data
-from student.generate.company import get_company_data
-from student.generate.student import get_student_data
-from student.models import Student, Company
+from activity.models import ActivityStudent
+from activity.test.base_test.activity_student import ActivityStudentBaseTest
 from util import result_util
 
 
-class TestActivityStudentList(TestCase):
+class TestActivityStudentList(ActivityStudentBaseTest):
     """
     分页获取活动校友测试
 
     :author: gexuewen
     :date: 2020/01/02
     """
-
-    activities_data = []
-    activities = []
-    students_data = []
-    students = []
-    activity_to_add = None
-    activity_students = []
+    def __init__(self, method_name):
+        super(TestActivityStudentList, self).__init__(method_name)
+        self.activity_to_add = None
+        self.activity_students = []
 
     def setUp(self):
-        # 生成三个活动
-        for i in range(0, 3):
-            activity_data = get_activity_data()
-            activity = Activity(**activity_data)
-            activity.save()
-            self.activities_data.append(activity_data)
-            self.activities.append(activity)
-        # 生成8个学生
-        company_data = get_company_data()
-        company = Company(**company_data)
-        company.save()
-        for i in range(0, 8):
-            # make pylint happy
-            if i == -1:
-                pass
-            student_data = get_student_data()
-            student = Student(company=company, **student_data)
-            student.save()
-            self.students_data.append(student_data)
-            self.students.append(student)
-        self.activity_to_add = self.activities[1]
+        super(TestActivityStudentList, self).setUp()
+        self.activity_to_add = self.activities[0]
         # 将前6个学生加入第2个活动中
         for student in self.students:
             activity_student_data = {
