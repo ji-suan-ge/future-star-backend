f8527c75162c73336fcb161aead9d40e6b31ad32
郑力煽 <602868253@qq.com>
2020-01-04 15:57:28 +0800

    Feat(course): finish add teacher
    Close #81

--- course/views/teacher.py
+++ course/views/teacher.py
@@ -7,30 +7,30 @@ resource
 from rest_framework import mixins, generics
 
 from course.models import Resource
-from course.serializers import ResourceSerializer
+from course.serializers import TeacherSerializer
 from util import result_util
 from util.pagination import CustomPageNumberPagination
 
 
-class ResourceViewSet(mixins.ListModelMixin,
-                      mixins.CreateModelMixin,
-                      generics.GenericAPIView):
+class TeacherViewSet(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     generics.GenericAPIView):
     """
-    resource view set
+    teacher view set
 
     :author: lishanZheng
-    :date: 2020/01/03
+    :date: 2020/01/04
     """
     queryset = Resource.objects.all()
-    serializer_class = ResourceSerializer
+    serializer_class = TeacherSerializer
     pagination_class = CustomPageNumberPagination
 
     def post(self, request):
         """
-        add resource
+        add teacher
 
         :author: lishanZheng
-        :date: 2020/01/03
+        :date: 2020/01/04
         """
         res = self.create(request)
         return result_util.success(res.data)
