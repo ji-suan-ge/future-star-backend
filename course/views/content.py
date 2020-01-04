33a1a20e3518c337c80caa6891b2f4b6145c59bf
郑力煽 <602868253@qq.com>
2020-01-04 17:46:22 +0800

    Feat(course): finish get content list
    Close #87

--- course/views/content.py
+++ course/views/content.py
@@ -21,9 +21,15 @@ class ContentViewSet(mixins.ListModelMixin,
     :author: lishanZheng
     :date: 2020/01/04
     """
-    queryset = Content.objects.all()
     serializer_class = ContentSerializer
     pagination_class = CustomPageNumberPagination
+
+    def get_queryset(self):
+        queryset = Content.objects.all()
+        course_id = self.request.GET.get('course_id')
+        if course_id is not None:
+            queryset = Content.objects.filter(course_id__in=course_id)
+        return queryset
 
     def post(self, request):
         """
@@ -34,3 +40,13 @@ class ContentViewSet(mixins.ListModelMixin,
         """
         res = self.create(request)
         return result_util.success(res.data)
+
+    def get(self, request):
+        """
+        get content list
+
+        :author: lishanZheng
+        :date: 2020/01/04
+        """
+        page = self.list(request).data
+        return result_util.success(page)
