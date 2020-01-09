6be1e6bc26ada7096ff52c84ed0625f2b6722d93
郑力煽 <602868253@qq.com>
2020-01-09 20:17:22 +0800

    Fix(config): modify config
    Ref #133

--- config/urls.py
+++ config/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.views.static import serve
-
-from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
