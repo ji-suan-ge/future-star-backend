fad73d737ffc249a3166197aff95648e73f487e4
xuewenG <gexuewen@outlook.com>
2020-01-09 14:04:06 +0800

    Feat(activity): modify activity module models
    Ref #127

--- activity/constant/activity_state.py
+++ activity/constant/activity_state.py
@@ -4,5 +4,15 @@ activity state
 :author: gexuewen
 :date: 2020/01/03
 """
-OPEN_ACTIVITY = 0
-CLOSED_ACTIVITY = 1
+# 没有开始报名
+PUBLISHED = 0
+# 报名中
+ENROLLING = 1
+# 招生结束没开始
+WAIT_FOR_START = 2
+# 进行中
+UNDERWAY = 3
+# 已结束
+CLOSED = 4
+# 已取消
+CANCELED = 5
--- activity/models.py
+++ activity/models.py
@@ -6,8 +6,7 @@ activity models
 """
 from django.db import models
 
-from activity.constant.activity_state import OPEN_ACTIVITY
-from activity.constant.activity_student_state import WAIT_FOR_PAY, PAID
+from activity.constant import activity_state, activity_student_state
 from clazz.models import Clazz
 from student.models import Student
 
@@ -19,28 +18,37 @@ class Activity(models.Model):
     :author: gexuewen
     :date: 2019/12/28
     """
+    ACTIVITY_STATE_CHOICE = (
+        (activity_state.PUBLISHED, '未开始报名'),
+        (activity_state.ENROLLING, '报名中'),
+        (activity_state.WAIT_FOR_START, '招生结束没开始'),
+        (activity_state.UNDERWAY, '进行中'),
+        (activity_state.CLOSED, '已结束'),
+        (activity_state.CANCELED, '已取消')
+    )
     # 活动名
-    name = models.CharField(max_length=30, blank=False)
+    name = models.CharField(max_length=30)
     # 报名开始时间
-    enroll_start_time = models.DateField(blank=False)
+    enroll_start_time = models.DateField()
     # 报名结束时间
-    enroll_end_time = models.DateField(blank=False)
+    enroll_end_time = models.DateField()
     # 主办方
-    organizer = models.CharField(max_length=30, blank=False)
+    organizer = models.CharField(max_length=30)
     # 开始时间
-    start_time = models.DateField(blank=False)
+    start_time = models.DateField()
     # 活动地址
-    address = models.CharField(max_length=100, blank=False)
+    address = models.CharField(max_length=100)
     # 活动日程
-    arrangement = models.TextField(max_length=1000, blank=False)
+    arrangement = models.TextField(max_length=1000)
     # 报名费
-    price = models.FloatField(blank=False)
+    price = models.IntegerField()
     # 当前人数
-    current_people_number = models.IntegerField(blank=False, default=0)
+    current_people_number = models.IntegerField(default=0)
     # 人数限制
-    people_number_limit = models.IntegerField(blank=False, default=0)
+    people_number_limit = models.IntegerField()
     # 活动状态
-    state = models.IntegerField(default=OPEN_ACTIVITY)
+    state = models.IntegerField(choices=ACTIVITY_STATE_CHOICE,
+                                default=activity_state.PUBLISHED)
 
 
 class ActivityStudent(models.Model):
@@ -51,15 +59,16 @@ class ActivityStudent(models.Model):
     :date: 2019/12/28
     """
     STATE_CHOICE = (
-        (WAIT_FOR_PAY, 'WAIT_FOR_PAY'),
-        (PAID, 'PAID')
+        (activity_student_state.WAIT_FOR_PAY, 'WAIT_FOR_PAY'),
+        (activity_student_state.PAID, 'PAID')
     )
     # 活动
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
     # 校友
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     # 缴费状态
-    state = models.IntegerField(choices=STATE_CHOICE)
+    state = models.IntegerField(choices=STATE_CHOICE,
+                                default=activity_student_state.WAIT_FOR_PAY)
 
 
 class ActivityClazz(models.Model):
